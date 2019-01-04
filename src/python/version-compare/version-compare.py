4f6ce318373af9ecf8c7e581661d168c387027ab
Tim Gurney aka Wolf <wolf@tgwolf.com>
2019-01-04 17:01:57 +0000

    More bandit fixes

--- src/python/version-compare/version-compare.py
+++ src/python/version-compare/version-compare.py
@@ -21,25 +21,28 @@ def run_tests():
     Tests
     """
 
-    assert version_compare("1", "1") == 0
-    assert version_compare("2.1", "2.2") < 0
-    assert version_compare("3.0.4.10", "3.0.4.2") > 0
-    assert version_compare("4.08", "4.08.01") < 0
-    assert version_compare("3.2.1.9.8144", "3.2") > 0
-    assert version_compare("3.2", "3.2.1.9.8144") < 0
-    assert version_compare("1.2", "2.1") < 0
-    assert version_compare("2.1", "1.2") > 0
-    assert version_compare("5.6.7", "5.6.7") == 0
-    assert version_compare("1.01.1", "1.1.1") == 0
-    assert version_compare("1.1.1", "1.01.1") == 0
-    assert version_compare("1", "1.0") == 0
-    assert version_compare("1.0", "1") == 0
-    assert version_compare("1.0", "1.0.1") < 0
-    assert version_compare("1.0.1", "1.0") > 0
-    assert version_compare("1.0.2.0", "1.0.2") == 0
+    assert version_compare("1", "1") == 0					# nosec
+    assert version_compare("2.1", "2.2") < 0					# nosec
+    assert version_compare("3.0.4.10", "3.0.4.2") > 0				# nosec
+    assert version_compare("4.08", "4.08.01") < 0				# nosec
+    assert version_compare("3.2.1.9.8144", "3.2") > 0				# nosec
+    assert version_compare("3.2", "3.2.1.9.8144") < 0				# nosec
+    assert version_compare("1.2", "2.1") < 0					# nosec
+    assert version_compare("2.1", "1.2") > 0					# nosec
+    assert version_compare("5.6.7", "5.6.7") == 0				# nosec
+    assert version_compare("1.01.1", "1.1.1") == 0				# nosec
+    assert version_compare("1.1.1", "1.01.1") == 0				# nosec
+    assert version_compare("1", "1.0") == 0					# nosec
+    assert version_compare("1.0", "1") == 0					# nosec
+    assert version_compare("1.0", "1.0.1") < 0					# nosec
+    assert version_compare("1.0.1", "1.0") > 0					# nosec
+    assert version_compare("1.0.2.0", "1.0.2") == 0				# nosec
 
+    #
+    # The following should fail
+    #
     try:
-        assert version_compare("1.0.1", "1.0") < 0				# This SHOULD fail
+        assert version_compare("1.0.1", "1.0") < 0				# nosec
     except AssertionError:
         print("It failed as expected")
 
